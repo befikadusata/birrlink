c0f99e60301d8a0f45942a3c861f375ac15cf314
Befikadu Sata <befikadusata@gmail.com>
2024-12-07 09:35:00 +0300

    custom user model

--- birrlink/birrlink/settings.py
+++ birrlink/birrlink/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -85,6 +86,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "core.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
